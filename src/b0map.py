8ac6e73b4307bfc10f52032340d265b6cf61b13b
Robert Peltekov <rpeltekov@gmail.com>
2023-11-07 00:36:19 -0800

    pipeline for getting off resonance maps from 2 te scans works now.mostly finished

--- src/b0map.py
+++ src/b0map.py
@@ -7,15 +7,77 @@ import matplotlib.pyplot as plt
 import argparse
 import sys
 
+def MRPhaseUnwrap(theta, voxelsize=[1.5, 1.5, 1.6], padsize=[12, 12, 12]):
+    # Pad the input data with zeros
+    theta = np.pad(theta, [(padsize[0], padsize[0]), (padsize[1], padsize[1]),
+                           (padsize[2], padsize[2]), (0,0)], mode='constant')
+
+    # Get the size of the padded data
+    NP = theta.shape
+    if len(theta.shape) == 3:
+        print(theta.shape)
+        print("we should not be here")
+        theta = theta[:,:,:,np.newaxis]
+        NP = theta.shape
+
+    # Make the size of the third dimension even
+    if theta.shape[2] % 2 == 1:
+        print("we should not be here")
+        theta = np.concatenate((theta, np.zeros_like(theta[:, :, -1, np.newaxis])), axis=2)
+
+    # Create coordinates and wave numbers
+    yy, xx, zz = np.meshgrid(np.arange(1, NP[1] + 1), 
+                             np.arange(1, NP[0] + 1), 
+                             np.arange(1, NP[2] + 1))
+    if len(voxelsize) > 1:
+        voxelsize = np.array(voxelsize)
+
+    FOV = voxelsize * NP[0:3]
+    xx = (xx - NP[0] / 2 - 1) / FOV[0]
+    yy = (yy - NP[1] / 2 - 1) / FOV[1]
+    zz = (zz - NP[2] / 2 - 1) / FOV[2]
+    k2 = xx**2 + yy**2 + zz**2
+
+    # Initialize arrays for phi and Laplacian
+    phi = np.zeros(NP)
+    Laplacian = np.zeros(NP).astype(np.cdouble)
+
+    def ifftnc(d):
+        scale = np.sqrt(len(d.flatten()))
+        return np.fft.fftshift(np.fft.ifftn(np.fft.fftshift(d)))*scale
+
+    def fftnc(d):
+        scale = np.sqrt(len(d.flatten()))
+        return np.fft.fftshift(np.fft.fftn(np.fft.fftshift(d)))/scale
+
+    for iii in range(NP[3]):
+        Laplacian0 = np.cos(theta[:,:,:,iii]) * ifftnc(k2 * fftnc(np.sin(theta[:,:,:,iii])))
+        Laplacian0 = Laplacian0 - np.sin(theta[:,:,:,iii]) * ifftnc(k2*fftnc(np.cos(theta[:,:,:,iii])))
+        Laplacian[:,:,:,iii] = Laplacian0
+        phi0 = fftnc(Laplacian0) / k2
+        phi0[NP[0] // 2, NP[1] // 2, NP[2] // 2] = 0
+        phi0 = np.real(ifftnc(phi0))
+        phi[:,:,:,iii] = phi0
+        print(f'done w array {iii} ')
+
+    print('')
+
+    # Remove padding to get the final results
+    phi = phi[padsize[0]:-padsize[0], padsize[1]:-padsize[1], padsize[2]:-padsize[2], :]
+    Laplacian = Laplacian[padsize[0]:-padsize[0], padsize[1]:-padsize[1], padsize[2]:-padsize[2], :]
+
+    return phi, Laplacian
+
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="2 folders of phase compute"
-                                                 +" phasemap")
+    parser = argparse.ArgumentParser(description="3d basis")
     parser.add_argument("folder1", type=str,default=0, help="")
     parser.add_argument("folder2", type=str,default=0, help="")
-    #parser.add_argument("o", type=str,default=0, help="")
-    #parser.add_argument("thresh", type=int,default=0, help="% value 0 to 100")
+    parser.add_argument("out", type=str,default=0, help="")
+    parser.add_argument("--voxX", type=int,default=1.5, help="")
+    parser.add_argument("--voxY", type=int,default=1.5, help="")
+    parser.add_argument("--voxZ", type=int,default=1.6, help="")
     parser.add_argument("--debug", action="store_true", help="")
 
     args = parser.parse_args()
@@ -27,58 +89,123 @@ if __name__ == "__main__":
     if not os.path.exists(b0_dir):
         os.makedirs(b0_dir)
 
-    #o_filename = args.o
+    tes = np.zeros(2)
+    for file_name in os.listdir(folder1):
+        if file_name.endswith("_3dPhase.npy"):
+            # Extract TE value from the file name
+            tes[0] = float(file_name.split("_")[1])
+            tes[1] = float(file_name.split("_")[2])
+            # Load and store the phase array
+            phases1 = np.load(os.path.join(folder1, file_name))
+         
+            phases2 = np.load(os.path.join(folder2, file_name))
 
-    num_slice = len(os.listdir(folder1)) // 2
-    phase_pref = "ph.MRDC."
-    mag_pref = "mag.MRDC."
+    # unwrap
+    unwrapf1, lap1 = MRPhaseUnwrap(phases1, [args.voxX, args.voxY, args.voxZ])
+    unwrapf2, lap2 = MRPhaseUnwrap(phases2, [args.voxX, args.voxY, args.voxZ])
+    #plt.imshow(unwrapf1[:,:,60,0], cmap='gray')
+    #plt.show()
 
-    for i in range(num_slice):
+    delt_phase1 = unwrapf1[:,:,:,1] - unwrapf1[:,:,:,0]
+    delt_phase2 = unwrapf2[:,:,:,1] - unwrapf2[:,:,:,0]
+    print("delt phase shape", delt_phase1.shape)
+    print("delt phase shape", delt_phase2.shape)
 
-        mag1file = os.path.join(folder1, mag_pref+str(i))
-        phase1file = os.path.join(folder1, phase_pref+str(i))
-        phase2file = os.path.join(folder2, phase_pref+str(i))
+    deltfreq1 = delt_phase1 / (2*np.pi*(tes[1]-tes[0])*10e-3)
+    deltfreq2 = delt_phase2 / (2*np.pi*(tes[1]-tes[0])*10e-3)
 
-        print(mag1file)
-        mag1 = dcmread(mag1file)
-        phase1 = dcmread(phase1file)
-        phase2 = dcmread(phase2file)
-        
-        mag1a = mag1.pixel_array
-        phase1a = phase1.pixel_array
-        phase2a = phase2.pixel_array
-
-        te1 = phase1[0x0018, 0x0081].value
-        te2 = phase2[0x0018, 0x0081].value
-        print(f"[INFO] TE1 = {te1} TE2 = {te2}")
-
-        phase1a = phase1a * np.pi / 32767
-        phase2a = phase2a * np.pi / 32767
-
-        delt_phase = phase2a - phase1a
-
-        m, n = mag1a.shape
-        mask = (mag1a > np.max(np.abs(mag1a)) / 10).astype(int)
-        
-        deltafreq = delt_phase / (2*np.pi*(te2-te1)*10e-3)
-
-        gradient = np.linspace(0, 1, 256)
-        colors = np.zeros((256, 3))
-        colors[:, 1] = gradient  # Green channel from 0 to 1
-        colors[:, 0] = 1 - gradient  # Red channel from 1 to 0
-
-        plt.imshow(deltafreq, cmap='gray')
-        plt.colorbar()
+    for slice in range(deltfreq1.shape[2]):
+        fig, ax = plt.subplots(1,3, figsize=(15, 5))
+        im0 = ax[0].imshow(deltfreq1[:,:,slice], cmap='gray')
+        ax[0].set_title(f"Hz of {args.folder1}")
+        fig.colorbar(im0, ax=ax[0])
+        im1 = ax[1].imshow(deltfreq2[:,:,slice], cmap='gray')
+        ax[1].set_title(f"Hz of {args.folder2}")
+        fig.colorbar(im1, ax=ax[1])
+        im2 = ax[2].imshow(deltfreq2[:,:,slice]-deltfreq1[:,:,slice], cmap='gray')
+        fig.colorbar(im2, ax=ax[2])
+        ax[2].set_title(f"Hz difference") 
         plt.show()
+        #save
+        plt.close()
 
-        
-        
+#    for i in range(num_slice):
+#        delt_phase = phase2a - phase1a
+#
+#        m, n = mag1a.shape
+#        mask = (mag1a > np.max(np.abs(mag1a)) / 10).astype(int)
+#        
+#        deltafreq = delt_phase / (2*np.pi*(te2-te1)*10e-3)
+#
+#        gradient = np.linspace(0, 1, 256)
+#        colors = np.zeros((256, 3))
+#        colors[:, 1] = gradient  # Green channel from 0 to 1
+#        colors[:, 0] = 1 - gradient  # Red channel from 1 to 0
+#
+#        plt.imshow(deltafreq, cmap='gray')
+#        plt.colorbar()
+#        plt.show()
 
-        
-        
-
-
-
-
-
+#if __name__ == "__main__":
+#
+#    parser = argparse.ArgumentParser(description="2 folders of phase compute"
+#                                                 +" phasemap")
+#    parser.add_argument("folder1", type=str,default=0, help="")
+#    parser.add_argument("folder2", type=str,default=0, help="")
+#    #parser.add_argument("o", type=str,default=0, help="")
+#    #parser.add_argument("thresh", type=int,default=0, help="% value 0 to 100")
+#    parser.add_argument("--debug", action="store_true", help="")
+#
+#    args = parser.parse_args()
+#
+#    folder1 = os.path.join("processed", args.folder1)
+#    folder2 = os.path.join("processed", args.folder2)
+#    b0_fold = "b0maps"
+#    b0_dir = os.path.join(os.getcwd(), b0_fold)
+#    if not os.path.exists(b0_dir):
+#        os.makedirs(b0_dir)
+#
+#    #o_filename = args.o
+#
+#    num_slice = len(os.listdir(folder1)) // 2
+#    phase_pref = "ph.MRDC."
+#    mag_pref = "mag.MRDC."
+#
+#    for i in range(num_slice):
+#
+#        mag1file = os.path.join(folder1, mag_pref+str(i))
+#        phase1file = os.path.join(folder1, phase_pref+str(i))
+#        phase2file = os.path.join(folder2, phase_pref+str(i))
+#
+#        print(mag1file)
+#        mag1 = dcmread(mag1file)
+#        phase1 = dcmread(phase1file)
+#        phase2 = dcmread(phase2file)
+#        
+#        mag1a = mag1.pixel_array
+#        phase1a = phase1.pixel_array
+#        phase2a = phase2.pixel_array
+#
+#        te1 = phase1[0x0018, 0x0081].value
+#        te2 = phase2[0x0018, 0x0081].value
+#        print(f"[INFO] TE1 = {te1} TE2 = {te2}")
+#
+#        phase1a = phase1a * np.pi / 32767
+#        phase2a = phase2a * np.pi / 32767
+#
+#        delt_phase = phase2a - phase1a
+#
+#        m, n = mag1a.shape
+#        mask = (mag1a > np.max(np.abs(mag1a)) / 10).astype(int)
+#        
+#        deltafreq = delt_phase / (2*np.pi*(te2-te1)*10e-3)
+#
+#        gradient = np.linspace(0, 1, 256)
+#        colors = np.zeros((256, 3))
+#        colors[:, 1] = gradient  # Green channel from 0 to 1
+#        colors[:, 0] = 1 - gradient  # Red channel from 1 to 0
+#
+#        plt.imshow(deltafreq, cmap='gray')
+#        plt.colorbar()
+#        plt.show()
 
--- src/phase_from_comp.py
+++ src/phase_from_comp.py
@@ -12,6 +12,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Converting dicoms and "
                                                  +"getting their new phase")
     parser.add_argument("folder", type=str,default=0, help="")
+    parser.add_argument("echos", type=int,default=1, help="")
     parser.add_argument("--pic", action="store_true", help="")
     parser.add_argument("--save", action="store_true", help="")
     parser.add_argument("--nifti", action="store_true", help="")
@@ -25,103 +26,104 @@ if __name__ == "__main__":
     if args.save and not os.path.exists(proc_dir):
         os.makedirs(proc_dir)
 
-    nifti_fold = "niftis"
-    nifti_dir = os.path.join(folder, "niftis")
-    if args.nifti and not os.path.exists(nifti_dir):
-        os.makedirs(nifti_dir)
-
     pattern = r'\.(\d+)'
     
     for scan_folder in os.listdir(folder):
-        if (args.debug):
-            print(f"[DEBUG] We are skipping: {scan_folder in ['processed', 'niftis']}")
-        if (scan_folder in ["processed", "niftis"]):
+        if scan_folder in ("processed", "b0maps"):
             continue
         print(f"[INFO] Iterating on scan {scan_folder}")
 
         scan_dir = os.path.join(folder, scan_folder)
         save_dir = os.path.join(proc_dir, scan_folder)
-        nifti_save_dir = os.path.join(nifti_dir, scan_folder)
+
+        if not os.path.exists(save_dir):
+            print("[INFO] saving to:", save_dir)
+            os.makedirs(save_dir)
+
+        if len(os.listdir(scan_dir)) == 0:
+            print(scan_dir, "exiting")
 
         # separate the real and imaginary images from the dcm folder that they
         # interleaved in
 
-        if (args.save):
-            images = {}
-            names = {}
-            
-            for file_name in os.listdir(scan_dir):
-                file_path = os.path.join(scan_dir, file_name)
-                ds = dcmread(file_path)
-                file_index = int(re.findall(pattern, file_name)[0])
-                names[file_index] = file_name
-                images[file_index] = ds
-                #print(ds.pixel_array.dtype)
+        tes = []
+        images = {}
+        names = {}
+        
+        for file_name in os.listdir(scan_dir):
+            file_path = os.path.join(scan_dir, file_name)
+            ds = dcmread(file_path)
+            file_index = int(re.findall(pattern, file_name)[0])
+            names[file_index] = file_name
+            images[file_index] = ds
+            te = float(ds[0x0018, 0x0081].value)
+            if te not in tes:
+                tes.append(te)
 
+        sorted_items = sorted(images.items())
+        images_list = np.array([img.pixel_array for idx, img in sorted_items])
 
-            sorted_items = sorted(images.items())
-            images_list = [img.pixel_array for idx, img in sorted_items]
+        tes.sort()
 
-            # from looking at all of them, it looks like real and imaginary images are
-            # every 3rd and 4th image in order. 
-            #
-            # so if there are 16 slices, then the order from all of them will be 
-            # magnitude slice 1, phase slice 1, real slice 1, imag slice 1, mag slice 2
-            # ... etc
+        reals = np.array(images_list[2::4])
+        imags = np.array(images_list[3::4])
+        mags = np.array(images_list[::4])
 
-            # i think it goes real = 3rd and imag = 4th and not the other way around
+        real = np.moveaxis([reals[::2], reals[1::2]], 0, -1)
+        imag = np.moveaxis([imags[::2], imags[1::2]], 0, -1)
+        mags = np.moveaxis([mags[::2], mags[1::2]], 0, -1)
 
-            real = np.array(images_list[2::4])
-            imag = np.array(images_list[3::4])
-            og_phases = np.array(images_list[1::4])
+        phases = np.arctan2(imag, real)
 
-            og_phases = np.clip(og_phases, -4096, 4095)
-            og_phases = og_phases / np.max(np.abs(og_phases))
+        if (args.debug):
+            print(f"[DEBUG] Max {np.max(phase)} min {np.min(phase)}")
 
-            phase = np.arctan2(imag, real)
-            phase = phase / np.pi
-            phase = (phase * 32767).astype(np.int16)
+        testring = "".join(["te_"]+[f"{te}_" for te in tes])
+        name = testring + "3dPhase.npy"
+        phases = np.moveaxis(phases, 0, 2)
+        np.save(os.path.join(save_dir, name), phases)
 
-            if (args.debug):
-                print(f"[DEBUG] Max {np.max(phase)} min {np.min(phase)}")
+        namemag = testring + "3dMag.npy"
+        mags = np.moveaxis(mags, 0, 2)
+        np.save(os.path.join(save_dir, namemag), mags)
 
-
-            if not os.path.exists(save_dir):
-                os.makedirs(save_dir)
-            for i in range(0,16):
-                #save the new phases
-                images[i*4+2].PixelData = phase[i].tobytes()
-                images[i*4+2].save_as(os.path.join(save_dir, f"ph.MRDC.{i}"))
-                # save the magnitudes
-                images[i*4+1].save_as(os.path.join(save_dir, f"mag.MRDC.{i}"))
-
-        if (args.nifti):
-            if not os.path.exists(nifti_save_dir):
-                os.makedirs(nifti_save_dir)
-            dicom2nifti.convert_directory(save_dir, nifti_save_dir, reorient=True)
-
-        if (args.pic):
-            for i in range(0,16):
-                fig, ax = plt.subplots(2, 2, figsize=(10, 10))  # 1 row, 2 columns
-
-                im1 = ax[0,0].imshow(phase[i], cmap='gray')
-                ax[0,0].set_title('phase from arctan(imag/real)')
-                fig.colorbar(im1, ax=ax[0,0])  # Add a color bar for the first image
-
-                arr = og_phases[i]
-                im2 = ax[0,1].imshow(arr, cmap='gray')
-                ax[0,1].set_title('original phase image')
-                fig.colorbar(im2, ax=ax[0,1])  # Add a color bar for the first image
-
-                arr = images_list[i*4+2]
-                im4 = ax[1,0].imshow(arr, cmap='gray')
-                ax[1,0].set_title('original "real" image')
-
-                arr = images_list[i*4+3]
-                im5 = ax[1,1].imshow(arr, cmap='gray')
-                ax[1,1].set_title('original "imag" image')
-
-                plt.show()
+#        if not os.path.exists(save_dir):
+#            os.makedirs(save_dir)
+#        for i in range(0,16):
+#            #save the new phases
+#            images[i*4+2].PixelData = phase[i].tobytes()
+#            images[i*4+2].save_as(os.path.join(save_dir, f"ph.MRDC.{i}"))
+#            # save the magnitudes
+#            images[i*4+1].save_as(os.path.join(save_dir, f"mag.MRDC.{i}"))
+#
+#        if (args.nifti):
+#            if not os.path.exists(nifti_save_dir):
+#                os.makedirs(nifti_save_dir)
+#            dicom2nifti.convert_directory(save_dir, nifti_save_dir, reorient=True)
+#
+#        if (args.pic):
+#            #TODO not implemented correctly right now
+#            for i in range(0,16):
+#                fig, ax = plt.subplots(2, 2, figsize=(10, 10))  # 1 row, 2 columns
+#
+#                im1 = ax[0,0].imshow(phase[i], cmap='gray')
+#                ax[0,0].set_title('phase from arctan(imag/real)')
+#                fig.colorbar(im1, ax=ax[0,0])  # Add a color bar for the first image
+#
+#                arr = og_phases[i]
+#                im2 = ax[0,1].imshow(arr, cmap='gray')
+#                ax[0,1].set_title('original phase image')
+#                fig.colorbar(im2, ax=ax[0,1])  # Add a color bar for the first image
+#
+#                arr = images_list[i*4+2]
+#                im4 = ax[1,0].imshow(arr, cmap='gray')
+#                ax[1,0].set_title('original "real" image')
+#
+#                arr = images_list[i*4+3]
+#                im5 = ax[1,1].imshow(arr, cmap='gray')
+#                ax[1,1].set_title('original "imag" image')
+#
+#                plt.show()
 
 
 print("[INFO] Done.")
